3e4e4079c34d805600a15656c208e78169f0a37d
Ricky Martinez <RMartinezW16@gmail.com>
2023-12-27 14:57:31 -0500

    build it visibile

--- src/playground/visible.js
+++ src/playground/visible.js
@@ -1,25 +1,28 @@
-let toggle = false;
 
-const onShowDetails = () => {
-    toggle = !toggle
-    render()
-}
 
-const render = () => {
-    const template = 
-        <div>
-            <h1>Visibility Toggle</h1>
-            <button onClick={onShowDetails}>{toggle === true ? "Hide details" : "Show secret details"}</button>
-            {toggle && (
-                <div>
-                <p>Super secret details</p>
-                </div>
-            )}
-        </div>
 
-    ReactDOM.render(template, appRoot)       
-}
+// let toggle = false;
 
-const appRoot = document.getElementById("app");
+// const onShowDetails = () => {
+//     toggle = !toggle
+//     render()
+// }
 
-render()
+// const render = () => {
+//     const template = 
+//         <div>
+//             <h1>Visibility Toggle</h1>
+//             <button onClick={onShowDetails}>{toggle === true ? "Hide details" : "Show secret details"}</button>
+//             {toggle && (
+//                 <div>
+//                 <p>Super secret details</p>
+//                 </div>
+//             )}
+//         </div>
+
+//     ReactDOM.render(template, appRoot)       
+// }
+
+// const appRoot = document.getElementById("app");
+
+// render()
