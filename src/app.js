f3690c1e5bca1de9160f51df5ddbe7434aaa6292
Ricky Martinez <RMartinezW16@gmail.com>
2024-01-03 15:38:34 -0500

    copl guy

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ var template = <div><h1>Indecision App</h1> <p>This is a parafgraph tag</p></div
 
 var template2 = 
     <div>
-        <h1>Ricky Martinez!</h1>
+        <h1>Ricky Martinez, the cool guy!</h1>
         <p>Age: 32</p>
         <p>Location: Brick, New Jersey</p>
 
