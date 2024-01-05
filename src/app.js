401e2cf055f4a2a894d9dd20a4d8b12f021f1a8a
Ricky Martinez <RMartinezW16@gmail.com>
2024-01-04 19:05:32 -0500

    es6

---
+++ src/app.js
@@ -0,0 +1,10 @@
+class Header extends React.Component{
+    render(){
+        <div>
+            <h1>Title</h1>
+            <h2>Sub title</h2>
+        </div>
+    }
+}
+
+ReactDOM.render(jas, document.getElementById("app"))
