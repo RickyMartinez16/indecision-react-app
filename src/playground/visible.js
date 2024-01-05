e16f17145acbad8cf854e62a9f88ed7026d1ddac
Ricky Martinez <RMartinezW16@gmail.com>
2024-01-05 17:37:33 -0500

    vis

--- src/playground/visible.js
+++ src/playground/visible.js
@@ -1,9 +1,13 @@
+const onShowDetails = (){
+
+}
+
 const render = () => {
     const template = 
         <div>
             <h1>Visibility Toggle</h1>
-            <button>Show Details</button>
-            
+            <button onClick={onShowDetails}>Show Details</button>
+
         </div>
 
     ReactDOM.render(template, appRoot)       
