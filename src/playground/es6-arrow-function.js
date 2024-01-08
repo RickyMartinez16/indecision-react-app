d530c2fe45d8d903319c4b0d795bd1781a15e8a9
Ricky Martinez <RMartinezW16@gmail.com>
2024-01-08 13:25:00 -0500

    const let

--- src/playground/es6-arrow-function.js
+++ src/playground/es6-arrow-function.js
@@ -1,1 +1,13 @@
 console.log("es6 arrow function js")
+
+const person = "Ricky Martinez"
+
+const getFirstName = (name) => {
+    return name.split(" ")[0]
+}
+
+const getFirstNameArrow = (name) => name.split(" ")[0]
+
+console.log(getFirstName(person))
+
+console.log(getFirstNameArrow(person))
