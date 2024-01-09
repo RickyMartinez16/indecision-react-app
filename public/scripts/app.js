f46006a77dcecbbf597deca1dd1aa76c183c4e69
Ricky Martinez <RMartinezW16@gmail.com>
2024-01-09 14:30:38 -0500

    playground 2

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,17 @@
 "use strict";
 
 console.log("arrow function 2");
+
+var multiplier = {
+    numbers: [1, 2, 3, 4, 5],
+    multiplyBy: 3,
+    multiply: function multiply() {
+        var _this = this;
+
+        return this.numbers.map(function (number) {
+            return number * _this.multiplyBy;
+        });
+    }
+};
+
+console.log(multiplier.multiply());
--- src/playground/es6-arrow-function2.js
+++ src/playground/es6-arrow-function2.js
@@ -1,1 +1,12 @@
 console.log("arrow function 2")
+
+
+const multiplier = {
+    numbers: [1, 2, 3, 4, 5],
+    multiplyBy: 3,
+    multiply() {
+        return this.numbers.map((number) => number * this.multiplyBy)
+    }
+}
+
+console.log(multiplier.multiply())
