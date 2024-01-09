d5dd643b19763243fad27dc05cbeee52008b906b
Ricky Martinez <RMartinezW16@gmail.com>
2024-01-09 14:27:24 -0500

    playground 2

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,17 +1,3 @@
 "use strict";
 
-console.log("es6 arrow function js");
-
-var person = "Ricky Martinez";
-
-var getFirstName = function getFirstName(name) {
-    return name.split(" ")[0];
-};
-
-var getFirstNameArrow = function getFirstNameArrow(name) {
-    return name.split(" ")[0];
-};
-
-console.log(getFirstName(person));
-
-console.log(getFirstNameArrow(person));
+console.log("arrow function 2");
