a12eb151f9de82b8ef4320f03749b2c50b21c82c
Ricky Martinez <RMartinezW16@gmail.com>
2024-01-09 14:39:50 -0500

    events

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,17 +1,74 @@
 "use strict";
 
-console.log("arrow function 2");
-
-var multiplier = {
-    numbers: [10, 20, 30],
-    multiplyBy: 3,
-    multiply: function multiply() {
-        var _this = this;
-
-        return this.numbers.map(function (number) {
-            return number * _this.multiplyBy;
-        });
-    }
+var app = {
+    title: "Fancy App",
+    subtitle: "The fanciest app you've ever seen.",
+    options: ["ONE", "TWO", "THREE"]
 };
 
-console.log(multiplier.multiply());
+var template1 = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        "p",
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        "p",
+        null,
+        app.options.length > 0 ? "Here are your options:" : "No options"
+    )
+);
+
+var user = {
+    name: "Ricky",
+    age: 32,
+    location: "Atlanta, GA"
+};
+
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            "p",
+            null,
+            "Location: ",
+            location
+        );
+    }
+}
+
+var template2 = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Template 2"
+    ),
+    React.createElement(
+        "h2",
+        null,
+        user.name ? user.name : "Anonymous"
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        "p",
+        null,
+        "Age: ",
+        user.age
+    ),
+    React.createElement(
+        "p",
+        null,
+        getLocation(user.location)
+    )
+);
+
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template1, appRoot);
