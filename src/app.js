198ea4d20253a2c8cb755d2e220c57256406a164
Ricky Martinez <RMartinezW16@gmail.com>
2024-01-10 15:38:49 -0500

    counter

--- src/app.js
+++ src/app.js
@@ -11,35 +11,4 @@ const template1 =
         <p>{app.options.length > 0 ? "Here are your options:" : "No options"}</p>
     </div>;
 
-
-
-let count = 0
-const add1 = () => {
-    count++
-    renderCounterApp()
-}
-const minus1 = () => {
-    count--
-    renderCounterApp()
-}
-const reset = () => {
-    count = 0
-    renderCounterApp()
-}
-
-
-const renderCounterApp = () => {
-    const template2 = 
-    <div>
-        <h1>Count: {count}</h1>
-        <button onClick={add1}>+1</button>
-        <button onClick={minus1}>-1</button>
-        <button onClick={reset}>RESET</button>
-    </div>
-
-    ReactDOM.render(template2, appRoot)
-}
-
 const appRoot = document.getElementById("app");
-
-renderCounterApp();
