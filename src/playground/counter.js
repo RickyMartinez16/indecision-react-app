46178378083874c11d0ea59b86e9ce7b403e4968
Ricky Martinez <RMartinezW16@gmail.com>
2024-01-15 15:23:54 -0500

    functional compennts

--- src/playground/counter.js
+++ src/playground/counter.js
@@ -5,7 +5,7 @@ class Counter extends React.Component {
         this.handleMinusOne = this.handleMinusOne.bind(this);
         this.handleReset = this.handleReset.bind(this);
         this.state = {
-            count: props.counter
+            count: props.count
         }
     }
 
@@ -50,34 +50,3 @@ Counter.defaultProps = {
 }
 
 ReactDOM.render(<Counter />, document.getElementById("app"))
-
-// let count = 0
-// const add1 = () => {
-//     count++
-//     renderCounterApp()
-// }
-// const minus1 = () => {
-//     count--
-//     renderCounterApp()
-// }
-// const reset = () => {
-//     count = 0    
-//     renderCounterApp()
-// }
-
-
-// const renderCounterApp = () => {
-//     const template2 = 
-//     <div>
-//         <h1>Count: {count}</h1>
-//         <button onClick={add1}>+1</button>
-//         <button onClick={minus1}>-1</button>
-//         <button onClick={reset}>RESET</button>
-//     </div>
-
-//     ReactDOM.render(template2, appRoot)
-// }
-
-// const appRoot = document.getElementById("app");
-
-// renderCounterApp();
