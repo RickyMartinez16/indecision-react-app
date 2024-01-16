c98140ba3912ad3ac02d2d58467f32f150aaf58f
Ricky Martinez <RMartinezW16@gmail.com>
2024-01-16 16:47:12 -0500

    lifestytle cylce

--- src/playground/counter.js
+++ src/playground/counter.js
@@ -5,7 +5,7 @@ class Counter extends React.Component {
         this.handleMinusOne = this.handleMinusOne.bind(this);
         this.handleReset = this.handleReset.bind(this);
         this.state = {
-            count: props.count
+            count: 0
         }
     }
 
