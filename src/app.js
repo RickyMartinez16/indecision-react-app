bea2a42d31cfa9dddc2bcb3668d5aabd3b1dcb20
Ricky Martinez <RMartinezW16@gmail.com>
2024-01-18 18:29:58 -0500

    exports

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@
 
 import {square, add} from "./utils.js"
 
-import {isAdult, canDrink} from "./person.js"
+import isSenior, {isAdult, canDrink} from "./person.js"
 
 console.log("app.js is running")
 
 
 console.log(isAdult(19))
 console.log(canDrink(22))
+console.log(isSenior(65))
--- src/person.js
+++ src/person.js
@@ -2,6 +2,6 @@ const isAdult = (x) => x >= 18 ? true : false
 
 const canDrink = (x) => x >= 21 ? true : false
 
-const isSenior = (x) => x >= 6 ? true : false
+export default (x) => x >= 6 ? true : false
 
 export { isAdult, canDrink}
