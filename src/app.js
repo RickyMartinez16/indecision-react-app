f72e7af97544f2f7709f8580a61ffbcd1ffeaf1f
Ricky Martinez <RMartinezW16@gmail.com>
2024-01-19 11:40:32 -0500

    one compoenent per file

--- src/app.js
+++ src/app.js
@@ -66,12 +66,6 @@ class IndecisionApp extends React.Component{
             return "This option already exists"
         }
 
-        // this.setState((prevState) => {
-        //     return {
-        //         options: prevState.options.concat([option])
-        //     }
-        // })
-
         this.setState((prevState) => ({options: prevState.options.concat([option])}))
     }
 
