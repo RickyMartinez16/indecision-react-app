650e434f0612c2463d69fa75d067288d3a4668d7
Ricky Martinez <RMartinezW16@gmail.com>
2024-01-19 11:38:15 -0500

    one compoenent per file

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom"
 import AddOption from "./components/AddOption";
 import Option from "./components/Option"
+import Header from "./components/Header"
 
 class IndecisionApp extends React.Component{
     constructor(props){
@@ -95,19 +96,6 @@ class IndecisionApp extends React.Component{
             </div>
         )
     }
-}
-
-const Header = (props) => {
-    return(
-        <div>
-            <h1>{props.title}</h1>
-            {props.subTitle && <h2>{props.subTitle}</h2>}
-        </div>
-    )
-}
-
-Header.defaultProps = {
-    title: "Indecision App"
 }
 
 const Action = (props) => {
