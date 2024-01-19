0c135e709ea7c770c5a961ec3232763e7a94f689
Ricky Martinez <RMartinezW16@gmail.com>
2024-01-19 15:48:11 -0500

    new syntax

--- src/app.js
+++ src/app.js
@@ -4,3 +4,22 @@ import IndecisionApp from "./components/IndecisionApp"
 
 
 ReactDOM.render(<IndecisionApp />, document.getElementById("app"))
+
+
+class OldSyntax {
+    constructor(){
+        this.name = "Ricky"
+    }
+}
+
+const oldSyntax = new OldSyntax()
+
+console.log(oldSyntax)
+
+class NewSyntax {
+    name = "Sam"
+}
+
+const newSyntax = new NewSyntax()
+
+console.log(newSyntax)
