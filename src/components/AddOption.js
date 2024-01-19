974da202582ac856f3f2fab9bd8a7a2414d6c16d
Ricky Martinez <RMartinezW16@gmail.com>
2024-01-19 11:53:51 -0500

    one compoenent per file

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -11,8 +11,6 @@ export default class AddOption extends React.Component{
 
     handleAddOption(e){
         e.preventDefault();
-
-        console.log("testing error")
         
         const option = e.target.elements.option.value.trim()
         const error = this.props.handleAddOption(option);
