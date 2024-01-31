c5a353af84b8c60466c47a06fafe8c5103a42f1b
Ricky Martinez <RMartinezW16@gmail.com>
2024-01-31 15:20:11 -0500

    modal

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Modal from "react-modal";
 
-const OptionModal = () => (
+const OptionModal = (props) => (
     <Modal
         isOpen={true}
         contentLabel="selected option"
