80c495ff5d3a80b103b6ef2b30dd0269683c0655
Ricky Martinez <RMartinezW16@gmail.com>
2024-01-31 15:18:27 -0500

    modal

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -3,12 +3,10 @@ import React from "react";
 import ReactModal from "react-modal";
 import Modal from "react-modal";
 
-const OptionModal = () => {
-    return (
-        <div>
-        some text
-        </div>
-    )
-}
+const OptionModal = () => (
+    <Modal>
+        <h3>Selected Options</h3>
+    </Modal>
+)
 
 export default OptionModal
