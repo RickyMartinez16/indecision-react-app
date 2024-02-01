87eb525eaeb83f61afcf93ac5adbe481e728ffc4
Ricky Martinez <RMartinezW16@gmail.com>
2024-02-01 14:15:56 -0500

    more filter drop down

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { connect } from "react-redux";
-import { setTextFilter, sortByAmount, sortByDate } from "../actions/filters";
+import React from "react"; // Importing React library
+import { connect } from "react-redux"; // Importing connect function from react-redux library
+import { setTextFilter, sortByAmount, sortByDate } from "../actions/filters"; // Importing action creators for filters
 
-const ExpenseListFilters = (props) => (
+const ExpenseListFilters = (props) => ( // Functional component for rendering expense list filters
     <div>
         <input 
             type="text" 
@@ -11,18 +11,18 @@ const ExpenseListFilters = (props) => (
                 props.dispatch(setTextFilter(e.target.value)) // Dispatching action to set text filter with the input value
             }}
         ></input>
-        <select
-            value={props.filters.sortBy}
-            onChange={(e) => {
-                if(e.target.value === "date"){
-                    props.dispatch(sortByDate())
-                } else if(e.target.value === "amount"){
-                    props.dispatch(sortByAmount())
+        <select // Dropdown menu for sorting options
+            value={props.filters.sortBy} // Value of the select dropdown is bound to the sortBy filter in the Redux store
+            onChange={(e) => { // Event handler for select change
+                if(e.target.value === "date"){ // If selected value is "date"
+                    props.dispatch(sortByDate()) // Dispatching action to sort by date
+                } else if(e.target.value === "amount"){ // If selected value is "amount"
+                    props.dispatch(sortByAmount()) // Dispatching action to sort by amount
                 }
             }}
         >
-            <option value="date">Date</option>
-            <option value="amount">Amount</option>
+            <option value="date">Date</option> {/* Option for sorting by date */}
+            <option value="amount">Amount</option> {/* Option for sorting by amount */}
         </select>
     </div>
 )
@@ -34,3 +34,4 @@ const mapStateToProps = (state) => { // Function to map Redux store state to com
 }
 
 export default connect(mapStateToProps)(ExpenseListFilters); // Connecting component to Redux store and exporting it
+ // Connecting component to Redux store and exporting it
