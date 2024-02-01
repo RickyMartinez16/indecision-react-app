50897e562e0223d5af3467fcc5ea9a7f8666a79b
Ricky Martinez <RMartinezW16@gmail.com>
2024-02-01 14:02:12 -0500

    comments

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,19 +1,23 @@
-import React from "react";
-import { connect } from "react-redux";
-import { setTextFilter } from "../actions/filters";
+import React from "react"; // Importing React library
+import { connect } from "react-redux"; // Importing connect function from react-redux library
+import { setTextFilter } from "../actions/filters"; // Importing action creator for setting text filter
 
-const ExpenseListFilters = (props) => (
+const ExpenseListFilters = (props) => ( // Functional component for expense list filters
     <div>
-        <input type="text" value={props.filters.text} onChange={(e) => {
-            props.dispatch(setTextFilter(e.target.value))
-        }}></input>
+        <input 
+            type="text" 
+            value={props.filters.text} // Value of the input field is bound to the text filter in the Redux store
+            onChange={(e) => { // Event handler for input change
+                props.dispatch(setTextFilter(e.target.value)) // Dispatching action to set text filter with the input value
+            }}
+        ></input>
     </div>
 )
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state) => { // Function to map Redux store state to component props
     return {
-        filters: state.filters
+        filters: state.filters // Mapping filters state from Redux store to component props
     }
 }
 
-export default connect(mapStateToProps)(ExpenseListFilters)
+export default connect(mapStateToProps)(ExpenseListFilters); // Connecting component to Redux store and exporting it
