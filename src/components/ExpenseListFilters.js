e8a61d1979184d482ebff8ccb5b0a3fd1d4517fd
Ricky Martinez <RMartinezW16@gmail.com>
2024-02-01 14:13:19 -0500

    more filter drop down

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -15,9 +15,9 @@ const ExpenseListFilters = (props) => (
             value={props.filters.sortBy}
             onChange={(e) => {
                 if(e.target.value === "date"){
-                    props.dispatch(sortByDate)
+                    props.dispatch(sortByDate())
                 } else if(e.target.value === "amount"){
-                    props.dispatch(sortByAmount)
+                    props.dispatch(sortByAmount())
                 }
             }}
         >
