0fb93c112ec32d2e2ce90465d51d2f3ece40f254
Ricky Martinez <RMartinezW16@gmail.com>
2024-02-03 22:42:46 -0500

    expense form

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
 class ExpenseForm extends Component {
+    state = {
+        description: ""
+    }
+
     render() {
         return (
             <div>
