7d34a295cc8f7a71895c1e5c6a492feaee23b60b
Ricky Martinez <RMartinezW16@gmail.com>
2024-02-03 22:39:27 -0500

    expense form

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -5,6 +5,8 @@ const ExpenseForm = () => (
         <form>
             <input type="text" placeholder="Description" autoFocus></input>
             <input type="number" placeholder="Amount"></input>
+            <textarea placeholder="Add a note for your expense (optional)"></textarea>
+            <button>Add Expense</button>
         </form>
     </div>
 )
