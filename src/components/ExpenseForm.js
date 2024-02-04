5b3d40a205a5e9ad40426823c6b5a8c948421ddf
Ricky Martinez <RMartinezW16@gmail.com>
2024-02-03 22:41:23 -0500

    expense form

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 
-const ExpenseForm = () => (
-    <div>
-        <form>
-            <input type="text" placeholder="Description" autoFocus></input>
-            <input type="number" placeholder="Amount"></input>
-            <textarea placeholder="Add a note for your expense (optional)"></textarea>
-            <button>Add Expense</button>
-        </form>
-    </div>
-)
+class ExpenseForm extends Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type="text" placeholder="Description" autoFocus></input>
+                    <input type="number" placeholder="Amount"></input>
+                    <textarea placeholder="Add a note for your expense (optional)"></textarea>
+                    <button>Add Expense</button>
+                </form>
+            </div>
+        );
+    }
+}
 
-export default ExpenseForm
+export default ExpenseForm;
