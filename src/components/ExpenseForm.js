749759afe82cb321c9272d2ac3fd8ba64c3d3661
Ricky Martinez <RMartinezW16@gmail.com>
2024-02-03 23:04:01 -0500

    expense form

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -19,7 +19,9 @@ class ExpenseForm extends Component {
 
     onAmountChange = (e) => {
         const amount = e.target.value
-        
+        if(amount.match(/^\d*(\.\d{0,2})?$/)){
+            this.setState(() => ({amount}))
+        }
     }
 
     render() {
