9a131591f137d4184ec3f970fef6891fadc82c53
Ricky Martinez <RMartinezW16@gmail.com>
2024-02-03 22:38:11 -0500

    expense form

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -4,6 +4,7 @@ const ExpenseForm = () => (
     <div>
         <form>
             <input type="text" placeholder="Description" autoFocus></input>
+            <input type="number" placeholder="Amount"></input>
         </form>
     </div>
 )
