720c6959e63a335a1a43574201d1557fad89c259
Ricky Martinez <RMartinezW16@gmail.com>
2024-02-03 22:49:09 -0500

    expense form

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -2,16 +2,43 @@ import React, { Component } from "react";
 
 class ExpenseForm extends Component {
     state = {
-        description: ""
+        description: "",
+        note: ""
+    }
+
+    onDescriptionChange = (e) => {
+        const description = e.target.value
+        this.setState(() => ({description}))
+    }
+
+    onNoteChange = (e) => {
+        const note = e.target.value
+        this.setState(() => ({note}))
     }
 
     render() {
         return (
             <div>
                 <form>
-                    <input type="text" placeholder="Description" autoFocus></input>
-                    <input type="number" placeholder="Amount"></input>
-                    <textarea placeholder="Add a note for your expense (optional)"></textarea>
+                    <input 
+                        type="text" 
+                        placeholder="Description" 
+                        autoFocus 
+                        value={this.state.description}
+                        onChange={this.onDescriptionChange}
+                    >
+                    </input>
+                    <input 
+                        type="number" 
+                        placeholder="Amount"
+                    >
+                    </input>
+                    <textarea 
+                        placeholder="Add a note for your expense (optional)"
+                        value={this.state.note}
+                        onChange={this.onNoteChange}
+                    >
+                    </textarea>
                     <button>Add Expense</button>
                 </form>
             </div>
