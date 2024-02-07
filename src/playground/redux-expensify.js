e825744bbf9d3648cd70076acd0bca945959fe02
Ricky Martinez <RMartinezW16@gmail.com>
2024-02-06 23:50:12 -0500

    reducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -129,8 +129,8 @@ store.subscribe(() => { // Subscribe to changes in the store and log the current
 
 // Dispatching actions--------------------
 
-// const expenseOne = store.dispatch(addExpense({description: "rent", amount: 100})); // Dispatching action to add an expense
-// const expenseTwo = store.dispatch(addExpense({description: "coffee", amount: 300})); // Dispatching action to add another expense
+const expenseOne = store.dispatch(addExpense({description: "rent", amount: 100})); // Dispatching action to add an expense
+const expenseTwo = store.dispatch(addExpense({description: "coffee", amount: 300})); // Dispatching action to add another expense
 
 // store.dispatch(removeExpense({id: expenseOne.expense.id})); // Dispatching action to remove an expense
 // store.dispatch(editExpense(expenseTwo.expense.id, {amount: 500})); // Dispatching action to edit an expense
@@ -138,9 +138,9 @@ store.subscribe(() => { // Subscribe to changes in the store and log the current
 // store.dispatch(setTextFilter()); // Dispatching action to clear text filter
 // store.dispatch(sortByAmount());
 // store.dispatch(sortByDate());
-store.dispatch(setStartDate(125))
-store.dispatch(setStartDate())
-store.dispatch(setEndDate(1250))
+// store.dispatch(setStartDate(125))
+// store.dispatch(setStartDate())
+// store.dispatch(setEndDate(1250))
 
 // Sample demo state
 const demoState = {
