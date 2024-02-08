a2399acab200390a0bfe4c82ff33eaabf55fe548
Ricky Martinez <RMartinezW16@gmail.com>
2024-02-08 14:33:08 -0500

    swtich

--- src/app.js
+++ src/app.js
@@ -32,15 +32,28 @@ const NotFoundPage = () => (
     </div>
 )
 
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <Link to="/">Home</Link>
+        <Link to="/create">Create</Link>
+        <Link to="/edit">Edit</Link>
+        <Link to="/help">Help</Link>
+    </header>
+)
+
 const routes = (
     <BrowserRouter>
-        <Switch>
-            <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-            <Route path="/create" component={AddExpensePage}/>
-            <Route path="/edit" component={EditExpensePage}/>
-            <Route path="/help" component={HelpPage}/>
-            <Route component={NotFoundPage}/>
-        </Switch>
+        <div>
+        <Header></Header>
+            <Switch>
+                <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+                <Route path="/create" component={AddExpensePage}/>
+                <Route path="/edit" component={EditExpensePage}/>
+                <Route path="/help" component={HelpPage}/>
+                <Route component={NotFoundPage}/>
+            </Switch>
+        </div>
     </BrowserRouter>
 )
 
