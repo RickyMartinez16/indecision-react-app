63cd6e05c297c813ce0f794f2302dd74291839ac
Ricky Martinez <RMartinezW16@gmail.com>
2024-02-09 17:58:12 -0500

    redux

--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -1,11 +1,29 @@
 import { createStore } from "redux";
 
 const store = createStore((state = { count: 0}, action) => {
-    return state
+    // if(action.type === "INCREMENT"){
+    //     return {
+    //         count:  state.count + 1
+    //     }
+    // } else {
+    //     return state
+    // }
+
+    switch (action.type){
+        case "INCREMENT":
+            return {
+                count:  state.count + 1
+            };
+        default : return state
+    }
 });
 
 console.log(store.getState());
 
+
+store.dispatch({
+    type: "INCREMENT"
+});
 
 store.dispatch({
     type: "INCREMENT"
