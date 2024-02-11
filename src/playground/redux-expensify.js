370fffb2e37a4d872530cde550c163b4134f21c5
Ricky Martinez <RMartinezW16@gmail.com>
2024-02-10 23:18:06 -0500

    reducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -97,6 +97,8 @@ store.dispatch(removeExpense({id: expenseOne.expense.id})); // Dispatching actio
 store.dispatch(editExpense(expenseTwo.expense.id, {amount: 500})); // Dispatching action to edit an expense
 store.dispatch(setTextFilter("rent")); // Dispatching action to set text filter
 store.dispatch(setTextFilter()); // Dispatching action to clear text filter
+store.dispatch(sortByAmount());
+store.dispatch(sortByDate());
 
 // Sample demo state
 const demoState = {
