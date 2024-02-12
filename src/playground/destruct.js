c94ad2390277ec0c8a1b47c70018e6636e5ef893
Ricky Martinez <RMartinezW16@gmail.com>
2024-02-12 15:34:39 -0500

    array destruct

--- src/playground/destruct.js
+++ src/playground/destruct.js
@@ -31,14 +31,14 @@
 
 //ARRAY------------------------
 
-const address = ["900 Battery Ave", "Atlanta", "Georgia", "30339"]
+// const address = ["900 Battery Ave", "Atlanta", "Georgia", "30339"]
 
-const [, city, state = "New Jersey",] = address;
+// const [, city, state = "New Jersey",] = address;
 
-console.log(`You are in ${city}, ${state}`)
+// console.log(`You are in ${city}, ${state}`)
 
 
 const coffee = ["Coffee (hot)", "2.00", "2.50", "3.00"]
-const [type, small, medium, large] = coffee
+const [type, , medium, ] = coffee
 
 console.log(`A medium ${type} costs ${medium}`)
