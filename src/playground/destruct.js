5015a209bd27d7f6b9e1b8f8a85ecf02b6dc8bb6
Ricky Martinez <RMartinezW16@gmail.com>
2024-02-12 15:20:25 -0500

    destruct

--- src/playground/destruct.js
+++ src/playground/destruct.js
@@ -1,15 +1,28 @@
-const person = {
-    name: "Ricky",
-    age: 33,
-    location: {
-        city: "ATL",
-        temp: 62
+// const person = {
+//     name: "Ricky",
+//     age: 32,
+//     location: {
+//         city: "ATL",
+//         temp: 62
+//     }
+// }
+
+// const {name = "Anonymous", age} = person
+
+// console.log(`${name} is ${age}`)
+
+// const {city, temp: temperature} = person.location
+// console.log(`it's ${temperature} in ${city}`)
+
+
+const book = {
+    title: "Ego is the enemy",
+    author: "Ryan Holiday",
+    publisher: {
+        name: "Penguin"
     }
 }
 
-const {name, age} = person
+const {name: publisherName = "Self-Published"} = book.publisher
 
-console.log(`${name} is ${age}`)
-
-const {city, temp} = person.location
-console.log(`it's ${temp} in ${city}`)
+console.log(`${publisherName}`)
