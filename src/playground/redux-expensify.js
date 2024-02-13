73c0e257e2d202461082f1bb85e5bd2689074e0a
Ricky Martinez <RMartinezW16@gmail.com>
2024-02-13 13:02:06 -0500

    reducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -8,5 +8,10 @@ const demoState = {
         amount: 54500,
         createdAt: 0
     }],
-    
+    filters: {
+        text: "rent",
+        sortBy: "amount", // date or amount
+        startDate: undefined,
+        endDate: undefined
+    }
 }
