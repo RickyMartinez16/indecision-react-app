086811189ce422b465c090aaa42069136a8ed1cc
Ricky Martinez <RMartinezW16@gmail.com>
2024-02-13 13:00:28 -0500

    reducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,1 +1,11 @@
 import { createStore, combineReducers } from "redux";
+
+const demoState = {
+    expenses: [{
+        id: "aekhflasjkhl",
+        description: "January rent",
+        note: "This wa the final payment for that address",
+        amount: 54500,
+        createdAt: 0
+    }]
+}
