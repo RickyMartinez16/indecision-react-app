f25a73755e09a063df8e8c5bb45f8a72693ed987
Ricky Martinez <RMartinezW16@gmail.com>
2024-02-14 16:25:57 -0500

    reconfigure files

--- src/playground/hoc.js
+++ src/playground/hoc.js
@@ -8,4 +8,25 @@ const Info = (props) => (
     </div>
 )
 
-ReactDOM.render(<Info info="These are the details"/>, document.getElementById("app"))
+const withAdminWarning = (WrappedComponent) => {
+    return (props) => (
+        <div>
+            {props.isAdmin && <p>This is private info. Please don't share</p>}
+            <WrappedComponent {...props}/>
+        </div>
+    )
+}
+
+const requireAuthentication = (WrappedComponent) => {
+    return (props) => (
+        <div>
+        {props.isAuthenticated ? <WrappedComponent {...props}/> : <p>please login to see the info</p>}
+        </div>
+    )
+}
+
+const AdminInfo = withAdminWarning(Info)
+const AuthInfo = requireAuthentication(Info)
+
+// ReactDOM.render(<AdminInfo isAdmin={true} info="These are the details"/>, document.getElementById("app"))
+ReactDOM.render(<AuthInfo isAuthenticated={true} info="These are the details"/>, document.getElementById("app"))
