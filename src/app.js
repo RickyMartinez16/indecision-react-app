51896afcaf6a5f3061ad87595a8bb2171ab9312d
Ricky Martinez <RMartinezW16@gmail.com>
2024-02-14 15:52:27 -0500

    reconfigure files

--- src/app.js
+++ src/app.js
@@ -2,10 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
+import { addExpense } from "./actions/expenses";
+import expensesReducer from "./reducers/expenses";
+import filtersReducer from "./reducers/filters";
+import { setTextFilter } from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses";
+
 
 const store = configureStore();
 
-console.log(store.getState())
+store.dispatch(addExpense({description: "Water Bill", amount: 8000, createdAt: 60})); // Dispatching action to add an expense
+store.dispatch(addExpense({description: "Gas Bill", amount: 7000, createdAt: 50}));
+
+store.dispatch(setTextFilter("water")); // Dispatching action to set text filter
+
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
 
 
 ReactDOM.render(<AppRouter />, document.getElementById("app"))
