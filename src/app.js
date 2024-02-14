5a9f7117ec165c5d01e32ec43f2edbe369f92bb4
Ricky Martinez <RMartinezW16@gmail.com>
2024-02-14 15:37:21 -0500

    reconfigure files

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+
+const store = configureStore();
+
+console.log(store.getState())
 
 
 ReactDOM.render(<AppRouter />, document.getElementById("app"))
