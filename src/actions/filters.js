a4f2520b7c92c1b06c57bc669380898f042dc595
Ricky Martinez <RMartinezW16@gmail.com>
2024-02-14 15:27:29 -0500

    reconfigure files

--- src/actions/filters.js
+++ src/actions/filters.js
@@ -1,29 +1,29 @@
 
 
 // Action creator for setting text filter
-const setTextFilter = (text = " ") => ({
+export const setTextFilter = (text = " ") => ({
     type: "SET_TEXT_FILTER", // Action type
     text // Text to be used as a filter (default is an empty string)
 });
 
 //Action creater for changing the sort by to date
-const sortByDate = (sortBy) => ({
+export const sortByDate = (sortBy) => ({
     type: "SORT_BY_DATE",
     sortBy
 })
 
 //Action creater for changing the sort by amount
-const sortByAmount = (sortBy) => ({
+export const sortByAmount = (sortBy) => ({
     type: "SORT_BY_AMOUNT",
     sortBy
 })
 
-const setStartDate = (startDate) => ({
+export const setStartDate = (startDate) => ({
     type: "SET_START_DATE",
     startDate
 })
 
-const setEndDate = (endDate) => ({
+export const setEndDate = (endDate) => ({
     type: "SET_END_DATE",
     endDate
 })
